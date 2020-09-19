dd90f94dc3c846ec110a4afc88660b8aa8655879
GitHub Action <action@github.com>
2020-09-19 18:00:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/flammehawk/intellijpugphp/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/flammehawk/intellijpugphp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.flammehawk.intellijpugphp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.flammehawk.intellijpugphp.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/flammehawk/intellijpugphp/services/MyProjectService.kt
+++ src/main/kotlin/com/github/flammehawk/intellijpugphp/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.flammehawk.intellijpugphp.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.flammehawk.intellijpugphp.MyBundle
 
 class MyProjectService(project: Project) {
 
